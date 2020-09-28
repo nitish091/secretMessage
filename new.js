4bf00a4ddf452de252169ca021209ae1c97a9ada
nitish091 <nitishrawat009@gmail.com>
2020-09-28 20:08:37 +0530

    initial commit

---
+++ new.js
@@ -0,0 +1,8 @@
+function fun(theChosenOne){
+    theChosenOne.innerHTML = "<h1>Bugger Off!!</h1>"
+}
+let theChosenOne = document.querySelector('.container');
+console.log(theChosenOne.innerHTML);
+theChosenOne.addEventListener('click', fun(theChosenOne));
+theChosenOne.addEventListener('mouseover', fun(theChosenOne));
+console.log("dafaq");
